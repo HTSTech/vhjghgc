89f87ea1652228b6c255664ce78fe40215231d84
HTSpankaj <pankaj.agade@htstechsolutions.com>
2022-09-08 01:41:01 +0530

    feat: Added OUTPUT.md programmatically

---
+++ pages/Footer/index.jsx
@@ -0,0 +1,40 @@
+
+  import { useEffect } from 'react';
+  import axios from 'axios';
+  import Link from 'next/link';
+  
+  export default function Footer() {
+  
+    useEffect(() => {
+      getData();
+    }, [])
+   
+    async function getData() {
+      
+      axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/9b937a0a-2f54-487e-a4a1-589af95ae076/vhjghgc/Footer/index.html").then(res=>{
+        console.log("res=> ",res);
+        document.getElementById("FooterID").innerHTML = res.data;
+        
+        //* css
+        axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/9b937a0a-2f54-487e-a4a1-589af95ae076/vhjghgc/Footer/style.css").then(CssRes=>{
+          console.log("CssRes", CssRes);
+          let createCssElement = document.createElement("style");
+          // createCssElement.nodeValue = CssRes.data;
+          createCssElement.appendChild(document.createTextNode(CssRes.data));
+          
+          document.getElementById("FooterID").appendChild(createCssElement)
+          axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/9b937a0a-2f54-487e-a4a1-589af95ae076/vhjghgc/Footer/script.js").then(JsRes=>{
+            console.log("JsRes", JsRes);
+            eval(JsRes.data)
+          })
+        })
+      })
+  
+    }
+    
+  
+    return (
+      <div id='FooterID'>
+      </div>
+    )
+  }
